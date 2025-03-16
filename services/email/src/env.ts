fdbb51b280004e5293aadc4ddb0ad73a954467d9
nferreira1 <nathan.1402@hotmail.com>
2025-03-16 02:07:33 -0300

    feat: serviço de disparo de e-mail com rabbitmq criado

---
+++ services/email/src/env.ts
@@ -0,0 +1,21 @@
+import { config } from "dotenv";
+import { resolve } from "path";
+import { z } from "zod";
+
+config({ path: resolve(__dirname, "../../../.env") });
+
+const envVariables = z.object({
+	BUN_ENV: z.union([z.literal("development"), z.literal("production")]),
+	RABBITMQ_USER: z.string(),
+	RABBITMQ_PASS: z.string(),
+	EMAIL_USER: z.string(),
+	EMAIL_PASS: z.string(),
+});
+
+const { error } = envVariables.safeParse(Bun.env);
+
+if (error) throw new Error("Erro na validação das variáveis de ambiente");
+
+export type Env = z.infer<typeof envVariables>;
+
+export const env: Env = Bun.env as unknown as Env;
---
+++ services/email/src/index.ts
@@ -0,0 +1,50 @@
+import { env } from "@/env";
+import * as amqp from "amqplib";
+import * as nodemailer from "nodemailer";
+
+const RABBITMQ_URL = `amqp://${env.RABBITMQ_USER}:${env.RABBITMQ_PASS}@${env.BUN_ENV === "development" ? "localhost" : "rabbitmq"}:5672`;
+const QUEUE_NAME = "email_queue";
+
+const transporter = nodemailer.createTransport({
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false,
+	auth: {
+		user: env.EMAIL_USER,
+		pass: env.EMAIL_PASS,
+	},
+});
+
+async function startConsumer() {
+	const connection = await amqp.connect(RABBITMQ_URL);
+	const channel = await connection.createChannel();
+	await channel.assertQueue(QUEUE_NAME, { durable: true });
+
+	console.log(
+		"✅ Email consumer is now running and listening for messages...",
+	);
+
+	channel.consume(QUEUE_NAME, async (msg) => {
+		if (msg) {
+			const emailData = JSON.parse(msg.content.toString());
+
+			console.log("📧 Sending email to:", emailData.to);
+
+			try {
+				await transporter.sendMail({
+					from: `Techcommerce <${env.EMAIL_USER}>`,
+					to: emailData.to,
+					subject: emailData.subject,
+					text: emailData.text,
+				});
+
+				console.log("✅ Email successfully sent!");
+				channel.ack(msg);
+			} catch (error) {
+				console.error("❌ Error sending email:", error);
+			}
+		}
+	});
+}
+
+startConsumer().catch(console.error);
